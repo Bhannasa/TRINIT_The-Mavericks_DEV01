a8d583c65bb64f5fc065dbea59f89740520f8d8e
Deepak-Kumar201 <dk10112002@gmail.com>
2022-01-30 02:59:47 +0530

    css selector added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import storage from "./FirebaseInit";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import Properties from "./Components/Properties";
+import { useState } from "react";
 
 function App() {
     const upload = async (type, elemId) => {
@@ -18,7 +20,17 @@ function App() {
         return url;
     };
 
-    return <div className="App"></div>;
+    return (
+      <div>
+        <Properties curSelected={curSelected} setCurSelected={setCurSelected} />
+        <div className="box" id="box" onClick={()=>{setCurSelected("box")}} style={{borderRadius:"30px"}}>
+          Hello World
+        </div>
+        <div className="box" id="box2" onClick={()=>{setCurSelected("box2")}} style={{borderRadius:"30px"}}>
+          Hello World
+        </div>
+      </div>
+    );
 }
 
 export default App;
--- src/classes/Css.js
+++ src/classes/Css.js
@@ -1,20 +1,50 @@
 class Css{
     constructor(){
-        this.height = '20px';
-        this.width = '20px';
-        this.top = '0px';
-        this.left = '0px';
-        this.color = 'black';
-        this.backgroundColor = 'white';
-        this.fontFamily = 'serif';
-        this.fontSize = '16px';
-        this.textAlign = 'left';
-        this.fontStyle = 'normal';
-        this.fontWeight = '100';
-        this.borderColor = 'black';
-        this.borderWidth = '0px';
-        this.borderRadius = '0px';
-        this.borderStyle = 'block';
+        this.noraml = {
+            "height" : "",
+            "width" : "",
+            "left" : "",
+            "top" : "",
+            "background-color" : "",
+            "border-radius" : "",
+            "font-size" : "",
+            "color" : "",
+            "font-family" :"",
+            "text-shadow":"",
+            "opacity":"",
+            "transition":"",
+            "padding":""
+        }
+        this.hover = {
+            "height" : "",
+            "width" : "",
+            "left" : "",
+            "top" : "",
+            "background-color" : "",
+            "border-radius" : "",
+            "font-size" : "",
+            "color" : "",
+            "font-family" :"",
+            "text-shadow":"",
+            "opacity":"",
+            "transition":"",
+            "padding":""
+        }
+        this.active = {
+            "height" : "",
+            "width" : "",
+            "left" : "",
+            "top" : "",
+            "background-color" : "",
+            "border-radius" : "",
+            "font-size" : "",
+            "color" : "",
+            "font-family" :"",
+            "text-shadow":"",
+            "opacity":"",
+            "transition":"",
+            "padding":""
+        }
     }
 }
 export default Css;
