0cb8cd04476d361486afeaf155b718520fef5905
Deepak-Kumar201 <dk10112002@gmail.com>
2022-01-29 18:45:40 +0530

    firebase storage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import "./App.css";
+import storage from "./FirebaseInit";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 function App() {
-  return (
-    <div className="App">
+    const upload = async (type, elemId) => {
+        elemId = document.getElementById(elemId);
+        const file = elemId.files[0];
+        var fileRef = ref(storage, file.name);
+        const metadata = {
+            contentType: type,
+        };
 
-    </div>
-  );
+        await uploadBytesResumable(fileRef, file, metadata);
+
+        const url = await getDownloadURL(fileRef);
+
+        return url;
+    };
+
+    return <div className="App"></div>;
 }
 
 export default App;
