83671e0818276dd44419fc314da033228dbb90ee
Bhannasa <adityabhan125@gmail.com>
2022-01-29 18:12:43 +0530

    css class

---
+++ src/classes/Css.js
@@ -0,0 +1,20 @@
+class Css{
+    constructor(){
+        this.height = '20px';
+        this.width = '20px';
+        this.top = '0px';
+        this.left = '0px';
+        this.color = 'black';
+        this.backgroundColor = 'white';
+        this.fontFamily = 'serif';
+        this.fontSize = '16px';
+        this.textAlign = 'left';
+        this.fontStyle = 'normal';
+        this.fontWeight = '100';
+        this.borderColor = 'black';
+        this.borderWidth = '0px';
+        this.borderRadius = '0px';
+        this.borderStyle = 'block';
+    }
+}
+export default Css;
